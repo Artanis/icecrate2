5e22af8d3e157654a383a60462a55aeaa8dfe5bc
Erik Youngren <artanis.00@gmail.com>
2014-09-28 00:05:41 -0700

    Add shopping lists.

--- icecrate/_static/js/dummy.js
+++ icecrate/_static/js/dummy.js
@@ -46,6 +46,9 @@ IcecrateDB.service('IcecrateDB', function ($window, $q, KeyGenerator) {
       if (db.objectStoreNames.contains('households')) {
         db.deleteObjectStore('households');
       }
+      if (db.objectStoreNames.contains('shopping')) {
+        db.deleteObjectStore('shopping');
+      }
 
       // create database
       var itemdb = db.createObjectStore('items', {'keyPath': "_id"});
@@ -54,6 +57,9 @@ IcecrateDB.service('IcecrateDB', function ($window, $q, KeyGenerator) {
       itemdb.createIndex('household,upc', ['household', 'upc'], {'unique': true});
 
       var householddb = db.createObjectStore('households', {"keyPath": "_id"});
+
+      var shoppingdb = db.createObjectStore('shopping', {"keyPath": "_id"});
+      shoppingdb.createIndex('household', 'household', {"unique": false});
     };
 
     request.onsuccess = function (e) {
@@ -334,16 +340,134 @@ IcecrateDB.service('IcecrateDB', function ($window, $q, KeyGenerator) {
 
     return deferred.promise;
   };
+
+  // Shopping Lists
+  this.all_shopping_lists = function () {
+    var deferred = $q.defer();
+
+    if (db === null) {
+      deferred.reject(
+        "(IndexedDB.all_shopping_lists) "+
+        "`icecrate.db` is not open.");
+    } else {
+      var transaction = db.transaction(['shopping'], 'readonly');
+      var store = transaction.objectStore('shopping');
+
+      var results = [];
+      var req = store.openCursor();
+      req.onsuccess = function (e) {
+        var cursor = e.target.result;
+
+        if (cursor) {
+          results.push(cursor.value);
+          cursor.continue();
+        } else {
+          deferred.resolve(results);
+        }
+      };
+      req.onerror = function (e) {
+        deferred.reject(e.value);
+      };
+    }
+
+    return deferred.promise;
+  };
+
+  this.get_shopping_lists_by_household = function (household_id) {
+    var deferred = $q.defer();
+
+    if (db === null) {
+      deferred.reject(
+        "(IndexedDB.get_shopping_lists_by_household) "+
+        "`icecrate.db` is not open.");
+    } else if (household_id === undefined) {
+      deferred.reject(
+        "(IndexedDB.get_shopping_lists_by_household) "+
+        "Expected `household_id`, got nothing.");
+    } else {
+      var transaction = db.transaction(['shopping'], 'readonly');
+      var store = transaction.objectStore('shopping');
+
+      var results = [];
+      var req = store.index('household').openCursor(household_id);
+      req.onsuccess = function (e) {
+        var cursor = e.target.result;
+
+        if(cursor) {
+          results.push(cursor.value);
+          cursor.continue();
+        } else {
+          deferred.resolve(results);
+        }
+      };
+      req.onerror = function (e) {
+        deferred.reject(e.value);
+      };
+    }
+
+    return deferred.promise;
+  };
+
+  this.get_shopping_list_by_id = function (_id) {
+    var deferred = $q.defer();
+
+    if (db === null) {
+      deferred.reject("(IcecrateDB.get_shopping_list_by_id) `icecrate.db` is not open.");
+    } else if (_id === undefined) {
+      deferred.reject("(IcecrateDB.get_shopping_list_by_id) Expected `_id`, got nothing.");
+    } else {
+      var transaction = db.transaction(['shopping'], 'readonly');
+      var store = transaction.objectStore('shopping');
+
+      var req = store.get(_id).onsuccess = function (e) {
+        deferred.resolve(e.target.result);
+      };
+    }
+
+    return deferred.promise;
+  };
+
+  this.update_shopping_list = function (shoppinglistdata) {
+    var deferred = $q.defer();
+
+    if (db === null) {
+      deferred.reject("(IndexedDB.update_shopping_list) `icecrate.db` is not open yet.");
+    } else if (shoppinglistdata === undefined) {
+      deferred.reject("(IndexedDB.update_shopping_list) Expected shopping list data, got nothing.");
+    } else {
+      var transaction = db.transaction(['shopping'], 'readwrite');
+      var store = transaction.objectStore('shopping');
+
+      var _id = $q.when(shoppinglistdata._id || KeyGenerator.uuid());
+      _id.then(
+        function (data) {
+        shoppinglistdata._id = data;
+      }).then(
+        function (data) {
+          var req = store.put(shoppinglistdata);
+          req.onsuccess = function (e) {
+            deferred.resolve(e.value);
+          };
+          req.onerror = function (e) {
+            deferred.reject(e.value);
+          };
+      });
+    }
+
+    return deferred.promise;
+  };
 });
 
 IcecrateDB.service('IcecrateDBSync', function ($q, $http, IcecrateDB) {
-  var items_url      = 'data/dummy/items.json';
-  var households_url = 'data/dummy/households.json';
+  var items_url         = 'data/dummy/items.json';
+  var households_url    = 'data/dummy/households.json';
+  var shopping_list_url = 'data/dummy/shopping.json';
 
   this.pull = function () {
     return $q.all(
       this.pull_items(),
-      this.pull_households());
+      this.pull_households(),
+      this.pull_shopping_lists());
   };
 
   this.pull_items = function () {
@@ -366,10 +490,21 @@ IcecrateDB.service('IcecrateDBSync', function ($q, $http, IcecrateDB) {
     });
   };
 
+  this.pull_shopping_lists = function () {
+    return $http.get(shopping_list_url).then(function (data) {
+      var lists = data.data;
+      for (var i in lists) {
+        var list = lists[i];
+        IcecrateDB.update_shopping_list(list);
+      }
+    });
+  };
+
   this.push = function () {
     return $q.all(
       this.push_items(),
-      this.push_households());
+      this.push_households(),
+      this.push_shopping_lists());
   };
 
   this.push_items = function () {
@@ -384,6 +519,15 @@ IcecrateDB.service('IcecrateDBSync', function ($q, $http, IcecrateDB) {
   this.push_housholds = function () {
     var msg =
       "(IcecrateDBSync.push_households) Changes not pushed to remote: "+
+      "using development data.";
+
+    console.log(msg);
+    return $q.reject(msg);
+  };
+
+  this.push_shopping_lists = function () {
+    var msg =
+      "(IcecrateDBSync.push_shopping_lists) Changes not pushed to remote: "+
       "using development data.";
 
     console.log(msg);
--- icecrate/_static/js/icecrate.js
+++ icecrate/_static/js/icecrate.js
@@ -68,22 +68,50 @@ Icecrate.controller('DetailItem', function($scope, IcecrateDB, $routeParams) {
   console.log($routeParams);
   var req = IcecrateDB.get_item_by_household_and_upc($routeParams.household_id, $routeParams.item_upc);
   req.then(function (data) {
-    console.log(data);
     $scope.item = data;
   });
-  // Items.then(function(data) {
-  //   for (var key in data) {
-  //     var item = data[key];
-  //     if(item.upc === $routeParams.item_upc && item.household === $routeParams.household_id) {
-  //       $scope.item = item;
-  //       break;
-  //     }
-  //   }
-  // })
+});
+
+Icecrate.controller('ShoppingLists', function($scope, IcecrateDB, $routeParams) {
+  if ($routeParams.household_id === undefined) {
+    // All Shopping lists
+    IcecrateDB.all_shopping_lists().then(function (data) {
+      $scope.shopping = data;
+    });
+  } else {
+    // Restrict to household
+    var req = IcecrateDB.get_shopping_lists_by_household($routeParams.household_id);
+    req.then(function (data) {
+      $scope.shopping = data;
+    });
+  }
+});
+
+Icecrate.controller('ShoppingListDetail', function ($scope, IcecrateDB, $routeParams) {
+  $scope.items = {}
+  $scope.list = undefined;
+  $scope.household = undefined;
+
+  var req = IcecrateDB.get_shopping_list_by_id($routeParams.list_id);
+  req.then(function (data) {
+    $scope.list = data;
+  });
+  req.then(function (data) {
+    var items = {};
+    for (var i in data.items) {
+      IcecrateDB.get_item_by_id(data.items[i]).then(function (item) {
+        items[item._id] = item;
+      });
+    }
+    $scope.items = items;
+  });
+  IcecrateDB.get_household_by_id($routeParams.household_id).then(function (data) {
+    $scope.household = data;
+  });
 });
 
 Icecrate.config(function ($routeProvider) {
-    // Household routes
+  // Household-scoped routes
   $routeProvider.when('/', {
     "controller":  "HouseholdList",
     "templateUrl": "templates/list-households.html"
@@ -94,20 +122,24 @@ Icecrate.config(function ($routeProvider) {
   });
   $routeProvider.when('/households/:household_id', {
     "controller":  "DetailHousehold",
-    "templateUrl": "templates/detauil-household.html"
+    "templateUrl": "templates/detail-household.html"
   });
   $routeProvider.when('/households/:household_id/items/:item_upc', {
     "controller":  "DetailItem",
     "templateUrl": "templates/detail-item.html"
   });
+  $routeProvider.when('/households/:household_id/lists/:list_id', {
+    "controller": "ShoppingListDetail",
+    "templateUrl": "templates/detail-shopping.html"
+  });
 
-  // Item (global) routes
+  // Globally-scoped routes
   $routeProvider.when('/items', {
     "controller":  "ItemList",
     "templateUrl": "templates/list-items.html"
   });
-  // $routeProvider.when('/items/:item_upc', {
-  //   "controller":  "DetailItem",
-  //   "templateUrl": "templates/detail-item.html"
-  // });
+  $routeProvider.when('/lists', {
+    "controller": "ShoppingLists",
+    "templateUrl": "templates/list-shopping.html"
+  });
 });
